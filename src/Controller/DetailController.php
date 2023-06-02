e360edfe055061ce639c3e63bc09bf20a1b67bf9
Hans <xllWizarDllx@gmail.com>
2023-06-02 03:54:52 +0200

    ajout des commentaire dans page details

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -8,18 +8,52 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Moto;
 use App\Entity\User;
 use App\Entity\Comments;
+use App\Form\MessageFormType;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Twig\Environment;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class DetailController extends AbstractController
 {
-    #[Route('/detail/{id}', name: 'detail')]
-    public function show(Environment $twig, Moto $moto): Response
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
     {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    #[Route('/detail/{id}', name: 'detail')]
+    public function show(Request $request, Environment $twig, Moto $moto, EntityManagerInterface $entityManager): Response
+    {
+        //récupération de la liste des commentaires associées à la moto
         $comments = $moto->getCommentairesMoto();
+
+        $comment = new Comments;
+
+        //récupération du token afin de récupéré l'utilisateur connecté
+        $token = $this->tokenStorage->getToken();
+
+        $form = $this->createForm(MessageFormType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $token->getUser();
+            $comment->setUserComments($user);
+            $comment->setMotoComments($moto);
+            $comment->setDate(new DateTime('now'));
+        
+            $entityManager->persist($comment);
+            $entityManager->flush();
+        
+        }
 
         return new Response($twig->render('detail/index.html.twig', [
             'moto' => $moto,
-            'comments' => $comments
+            'comments' => $comments,
+            'MessageForm' => $form->createView(),
         ]));
     }
 }
---
+++ src/Form/MessageFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comments;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class MessageFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('texte', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'cols' => 30,
+                    'rows' => 10,
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le message ne doit pas être vide',
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comments::class,
+        ]);
+    }
+}
